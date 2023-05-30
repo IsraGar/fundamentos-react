a9b9137f20653c5c299d58f7db512d3dd7deb87d
Israel Garcia <isragarciahdez@gmail.com>
2023-05-30 13:22:43 -0600

    Completar y eliminar TODOs

--- src/App.js
+++ src/App.js
@@ -28,7 +28,24 @@ function App() {
       return todoText.includes(searchText)
     }
   );
-  // console.log('Los usuarios buscan TODOs de ' + searchValue);
+  
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
 
   return (
     <>
@@ -43,6 +60,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>      
