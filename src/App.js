1aa8b8c4e9dbfe56b9e1f60534de65adb056f2eb
Israel Garcia <isragarciahdez@gmail.com>
2023-05-30 12:24:47 -0600

    Buscando TODOs

--- src/App.js
+++ src/App.js
@@ -20,6 +20,14 @@ function App() {
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    }
+  );
   // console.log('Los usuarios buscan TODOs de ' + searchValue);
 
   return (
@@ -30,7 +38,7 @@ function App() {
         setSearchValue={setSearchValue}
       />
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
