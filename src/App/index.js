edc5176f4120d6ff030edfa324c44c81098b559c
Israel Garcia <isragarciahdez@gmail.com>
2023-06-01 12:36:19 -0600

    Actualizando estados con useEffect

--- src/App/index.js
+++ src/App/index.js
@@ -12,7 +12,12 @@ import { AppUI } from './AppUI';
 
 function App() {
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -46,6 +51,8 @@ function App() {
 
   return (
     <AppUI
+      loading={loading}
+      error={error}
       completedTodos={completedTodos}
       totalTodos={totalTodos}
       searchValue={searchValue}
